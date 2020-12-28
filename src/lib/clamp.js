cb99e027c06776f55fcc4b653b81da69c4e32bb9
Mikel Parra <mikel.parra@beezy.net>
2020-12-28 16:46:38 +0100

    update element on useEffect

--- src/lib/clamp.js
+++ src/lib/clamp.js
@@ -1,6 +1,6 @@
-import React, { useState, Fragment } from 'react';
-import isCssEllipsisApplied from './utils/isCssEllipsisApplied';
-import TruncatedElement from './truncatedElement';
+import React, { useState, Fragment, useRef, useEffect } from "react";
+import isCssEllipsisApplied from "./utils/isCssEllipsisApplied";
+import TruncatedElement from "./truncatedElement";
 
 const Clamp = ({
     children,
@@ -9,46 +9,49 @@ const Clamp = ({
     withTooltip = true,
     withToggle = false,
     texts = {
-        showMore: 'More',
-        showLess: 'Less'
-    }
+        showMore: "More",
+        showLess: "Less",
+    },
 }) => {
+    const element = useRef();
     const [sLines, setLines] = useState(lines);
     const [showMore, setShowMore] = useState(false);
     const [isExpanded, setIsExpanded] = useState(false);
 
-    const shouldApplyTooltip = elem => {
-        return Boolean(isCssEllipsisApplied(elem) && withTooltip);
-    };
-
-    const handleShowToggle = () => {
-        if (withToggle) {
-            if (!showMore && !isExpanded) {
-                setShowMore(true);
-            }
-        }
-    };
-
-    const handleInitElement = elem => {
-        if (shouldApplyTooltip(elem)) {
-            const title = elem.textContent;
-            elem.setAttribute('title', title);
-        }
-
-        handleShowToggle();
-    };
-
-    const handleToggleShowMore = show => {
+    const handleToggleShowMore = (show) => {
         const newLines = show ? maxLines : lines;
 
-        setShowMore(showMore => !showMore);
-        setIsExpanded(isExpanded => !isExpanded);
+        setShowMore((showMore) => !showMore);
+        setIsExpanded((isExpanded) => !isExpanded);
         setLines(newLines);
     };
 
-    const handleGetRef = elem => {
-        if (elem) handleInitElement(elem);
+    const handleGetRef = (elem) => {
+        if (elem) {
+            element.current = elem;
+        }
     };
+
+    useEffect(() => {
+        const handleConfigElement = () => {
+            const elem = element.current;
+
+            if (isCssEllipsisApplied(elem) && withTooltip) {
+                const title = elem.textContent;
+                elem.setAttribute("title", title);
+            } else {
+                elem.removeAttribute("title");
+            }
+
+            if (withToggle) {
+                if (!showMore && !isExpanded) {
+                    setShowMore(true);
+                }
+            }
+        };
+
+        element.current && handleConfigElement();
+    }, [isExpanded, showMore, withToggle, withTooltip]);
 
     return (
         <Fragment>
