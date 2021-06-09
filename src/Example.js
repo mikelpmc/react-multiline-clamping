a864464ad99b53222107adccc2d2a2be11e8c37c
mikel <mikelpmc@gmail.com>
2021-06-09 17:05:04 +0200

    update example

--- src/Example.js
+++ src/Example.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import Clamp from "./lib";
+import React, { useState } from 'react';
+import Clamp from './lib';
 
 const initialText = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
 Illum perspiciatis ea consequuntur ipsum deleniti placeat
@@ -22,24 +22,34 @@ function Example() {
                     value={text}
                     cols={4}
                     rows={4}
-                    onChange={(ev) => setText(ev.target.value)}
-                    style={{ width: "100%", height: "150px" }}
+                    onChange={ev => setText(ev.target.value)}
+                    style={{ width: '100%', height: '150px' }}
                 />
             </div>
             <div>
                 <input
                     type="text"
                     value={lines}
-                    onChange={(ev) => setLines(ev.target.value)}
+                    onChange={ev => setLines(ev.target.value)}
                 />
             </div>
 
             <Clamp
-                texts={{ showMore: "Moreeee", showLess: "Menos" }}
+                showMoreElement={({ toggle }) => (
+                    <button type="button" onClick={toggle}>
+                        Show more
+                    </button>
+                )}
+                showLessElement={({ toggle }) => (
+                    <span type="button" onClick={toggle}>
+                        menossssss
+                    </span>
+                )}
+                texts={{ showMore: 'Moreeee', showLess: 'Menos' }}
                 lines={lines}
                 maxLines={8}
                 withToggle
-                onShowMore={(show) => console.log(show)}
+                onShowMore={show => console.log(show)}
             >
                 <p>{text}</p>
             </Clamp>
