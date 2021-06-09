e0faa3977c3ea96c5c6efc78c0d895d06175ce8b
mikel <mikelpmc@gmail.com>
2021-06-09 17:04:56 +0200

    add showElement props and remove buttons

--- src/lib/clamp.js
+++ src/lib/clamp.js
@@ -1,6 +1,6 @@
-import React, { useState, Fragment, useEffect } from "react";
-import isCssEllipsisApplied from "./utils/isCssEllipsisApplied";
-import TruncatedElement from "./truncatedElement";
+import React, { useState, Fragment, useEffect } from 'react';
+import isCssEllipsisApplied from './utils/isCssEllipsisApplied';
+import TruncatedElement from './truncatedElement';
 
 const Clamp = ({
     children,
@@ -8,40 +8,38 @@ const Clamp = ({
     maxLines = 8,
     withTooltip = true,
     withToggle = false,
-    texts = {
-        showMore: "More",
-        showLess: "Less",
-    },
-    onShowMore = () => {},
+    showMoreElement,
+    showLessElement,
+    onShowMore = () => {}
 }) => {
     const [sLines, setLines] = useState(lines);
     const [isExpanded, setIsExpanded] = useState(false);
     const [showMore, setShowMore] = useState(false);
 
-    const handleToggleShowMore = (show) => {
+    const handleToggleShowMore = show => {
         const newLines = show ? maxLines : lines;
 
-        setShowMore((showMore) => !showMore);
-        setIsExpanded((isExpanded) => !isExpanded);
+        setShowMore(showMore => !showMore);
+        setIsExpanded(isExpanded => !isExpanded);
         setLines(newLines);
 
         onShowMore(show);
     };
 
-    const handleConfigElement = (elem) => {
+    const handleConfigElement = elem => {
         if (!elem) return;
 
         if (isCssEllipsisApplied(elem)) {
             if (withTooltip) {
                 const title = elem.textContent;
-                elem.setAttribute("title", title);
+                elem.setAttribute('title', title);
             }
 
             if (withToggle && !showMore && !isExpanded) {
                 setShowMore(true);
             }
         } else {
-            elem.removeAttribute("title");
+            elem.removeAttribute('title');
             setShowMore(false);
         }
     };
@@ -58,23 +56,12 @@ const Clamp = ({
                 {children}
             </TruncatedElement>
 
-            {showMore && !isExpanded && (
-                <button
-                    type="button"
-                    onClick={() => handleToggleShowMore(true)}
-                >
-                    {texts.showMore}
-                </button>
-            )}
+            {showMore &&
+                !isExpanded &&
+                showMoreElement({ toggle: () => handleToggleShowMore(true) })}
 
-            {isExpanded && (
-                <button
-                    type="button"
-                    onClick={() => handleToggleShowMore(false)}
-                >
-                    {texts.showLess}
-                </button>
-            )}
+            {isExpanded &&
+                showLessElement({ toggle: () => handleToggleShowMore(false) })}
         </Fragment>
     );
 };
