e13b395676a5e5bdd17e0561bdb849faecae9c4d
Mikel Parra <mikel.parra@beezy.net>
2021-06-16 18:46:46 +0200

    feat: add compound components

--- src/Example.js
+++ src/Example.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Clamp from './lib';
+import React, { useState } from "react";
+import Clamp from "./lib";
 
 const initialText = `Lorem ipsum dolor sit amet consectetur adipisicing elit.
 Illum perspiciatis ea consequuntur ipsum deleniti placeat
@@ -11,49 +11,36 @@ dignissimos dolor facere officia delectus corrupti
 perferendis laboriosam deserunt nobis, suscipit autem atque?`;
 
 function Example() {
-    const [text, setText] = useState(initialText);
-    const [lines, setLines] = useState(2);
+  const [text, setText] = useState(initialText);
+  const [lines, setLines] = useState(2);
 
-    return (
-        <div>
-            <h1>Multiline clamp example</h1>
-            <div>
-                <textarea
-                    value={text}
-                    cols={4}
-                    rows={4}
-                    onChange={ev => setText(ev.target.value)}
-                    style={{ width: '100%', height: '150px' }}
-                />
-            </div>
-            <div>
-                <input
-                    type="text"
-                    value={lines}
-                    onChange={ev => setLines(ev.target.value)}
-                />
-            </div>
+  return (
+    <div>
+      <h1>Multiline clamp example</h1>
+      <div>
+        <textarea
+          value={text}
+          cols={4}
+          rows={4}
+          onChange={(ev) => setText(ev.target.value)}
+          style={{ width: "100%", height: "150px" }}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={lines}
+          onChange={(ev) => setLines(ev.target.value)}
+        />
+      </div>
 
-            <Clamp
-                showMoreElement={({ toggle }) => (
-                    <button type="button" onClick={toggle}>
-                        Show more
-                    </button>
-                )}
-                showLessElement={({ toggle }) => (
-                    <span type="button" onClick={toggle}>
-                        menossssss
-                    </span>
-                )}
-                lines={lines}
-                maxLines={8}
-                withToggle
-                onShowMore={show => console.log(show)}
-            >
-                <p>{text}</p>
-            </Clamp>
-        </div>
-    );
+      <Clamp lines={lines} maxLines={8}>
+        <Clamp.Text>
+          <p>{text}</p>
+        </Clamp.Text>
+      </Clamp>
+    </div>
+  );
 }
 
 export default Example;
